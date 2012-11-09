4097a6e9818c814f548a2c676b9030aefc85dfeb
jden <jason@denizac.org>
2012-11-08 16:54:23 -0800

    add dumb node wrapper

---
+++ static.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+var port = process.env.PORT || 5555;
+
+var server = http.createServer();
+
+server.on('request', function (req, res) {
+  fs.createReadStream('index.html')
+  .pipe(res)
+  .on('end', res.end);
+});
+
+server.listen(port);
